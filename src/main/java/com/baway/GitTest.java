6519d80335f4c88e3978c19439242b472c56372a
Alex <yulong0910@163.com>
2022-03-19 22:36:28 +0800

    first commit

---
+++ src/main/java/com/baway/GitTest.java
@@ -0,0 +1,15 @@
+package com.baway;
+
+/**
+ * @ author yulong
+ * @ createTime 2022-03-19 22:28
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello Git!");
+
+    }
+
+}
